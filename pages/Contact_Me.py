14377fc7b3c0ee2a128244e6ffa083b70e975d32
SiddharthSai4701 <sidvsai@gmail.com>
2024-08-17 20:13:28 +0530

    Modified contact me

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,5 +1,27 @@
 import streamlit as st
-from send_email import send_email
+# from send_email import send_email
+
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
+
+def send_email(message):
+
+    load_dotenv()
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "sidvsai@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        # Sending the mail to myself when a user wants to contact me. Therefore from and to addresses are the same
+        server.sendmail(username, username, message)
+        
 st.header("Contact Me")
 
 with st.form(key="email_form"):
